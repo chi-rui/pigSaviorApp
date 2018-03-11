85bd35b1df5638fcee3ff12d23e6231739e5b0a9
RayRaya <xic768210@yahoo.com.tw>
2018-03-11 14:07:08 +0800

    Create git repository.

---
+++ Assets/Scripts/InfinityBackground.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfinityBackground : MonoBehaviour {
+
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = GetComponent<Renderer>().material.mainTextureOffset.x;
+		GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(x-speed, 0));
+	}
+
+	// public float scrollSpeed;
+	// public float tileSize;
+
+	// private Vector2 startPosition;
+
+	// void Start ()
+	// {
+	//     startPosition = transform.position;
+	// }
+
+	// void Update ()
+	// {
+	//     float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+	//     transform.position = startPosition + new Vector2(-1f ,0f) * newPosition;
+	// }
+}
---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+	// Account info
+	private string account = null;
+	private string password = null;
+	private List<int> achienements = new List<int>();
+	private List<string> records = new List<string>();
+
+	// Game info
+	public static int nowStage = 1;
+	public static int progress = 0;
+	public static int character = 0;
+	
+	public int i;
+
+	// public static PlayerInfo CreateFromJSON(string jsonString){
+	// 	return JsonUtility.FromJson<PlayerInfo>(jsonString);
+	// }
+	
+	private void getInfo(){
+		if(account != ""){
+			Debug.Log("account: " + account + "\npassword: " + password);
+			Debug.Log("\nprogress: " + progress + "\ncharacter: " + character );
+			Debug.Log("\nachievements: \n");
+			for(i = 0; i < achienements.Count; i++){
+				Debug.Log(achienements[i] + "\n");
+			}
+			Debug.Log("\nrecords: \n");
+			for(i = 0; i < records.Count; i++){
+				Debug.Log(records[i] + "\n");
+			}
+		}
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		// nowStage = progress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public GameObject C;
+	public void setProgress(int value){
+		progress = value;
+		print("Progress already change to " + progress + ".");
+		// print(C.transform.position);
+		// C.transform.position = new Vector2(-1000,-1400);
+		// print(C.transform.position);		
+	}
+
+}
