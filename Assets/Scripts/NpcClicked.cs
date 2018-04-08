7e2512be282e2d6e1d29c6c8a77cb34188c4c8e3
Ray <rayray@Rayde-MBP.Home>
2018-04-08 22:28:19 +0800

    RPG scene character move, npc clicked and camera area limit done.

---
+++ Assets/Scripts/NpcClicked.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NpcClicked : MonoBehaviour {
+
+	public GameObject TalkWindow;
+	public Text title;
+	public List<string> correctPlot;
+	public List<string> wrongPlot;
+	public string npc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void npcClicked(){
+		TalkWindow.SetActive(true);
+		TalkWindow.transform.GetChild(1).GetComponentInChildren<Text>().text =  this.npc;
+		TalkWindow.transform.GetChild(2).GetComponentInChildren<Text>().text =  this.correctPlot[0];
+	} 
+}
