05d42aa290ace98f4b2f28df5f530af55ddd5848
eva19950630 <fun910347@gmail.com>
2018-04-24 20:57:26 +0800

    rhythm mode: pointer moving in a range

--- Assets/Scripts/Game/RhythmMode.cs
+++ Assets/Scripts/Game/RhythmMode.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class RhythmMode : MonoBehaviour {
 	public GameObject pointer;
+	public float speed;
 	
+	private Vector3 pos_L, pos_R;
 
 	// Use this for initialization
 	void Start () {
-		
+		pos_L = new Vector3(-1860f, pointer.transform.position.y, pointer.transform.position.z);
+		pos_R = new Vector3(-530f, pointer.transform.position.y, pointer.transform.position.z);
+		speed = 0.5f;
 	}
 	
 	// Update is called once per frame
-	// 540 1850
+	// -540 -1850
 	void Update () {
-		// pointer.transform.position += new Vector3(5f, 0, 0);
-		if (pointer.transform.position.x > -540) {
-			print("超過了！");
-			pointer.transform.position -= new Vector3(5f, 0, 0);
-		}
+		pointer.transform.position = Vector3.Lerp(pos_L, pos_R, Mathf.PingPong(speed * Time.time, 1.0f));
 	}
 }
